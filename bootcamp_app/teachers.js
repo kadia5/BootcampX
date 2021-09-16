5dfa22eb7c91f8b9464a40eff0589da07c3fd736
kadia <kdabraeux@gmail.com>
2021-09-16 20:41:36 +0000

    parametized queries

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -7,6 +7,21 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
+// pool.query(`
+// SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
+// FROM teachers
+// JOIN assistance_requests ON teacher_id = teachers.id
+// JOIN students ON student_id = students.id
+// JOIN cohorts ON cohort_id = cohorts.id
+// WHERE cohorts.name = 'JUL02'
+// ORDER BY teacher;
+// `)
+// .then(res => {
+//   console.log(res.rows)
+//   res.rows.forEach(row => {
+//     console.log(`${row.cohort}: ${row.teacher}`);
+//   })
+// }).catch(err => console.error('query error', err.stack));
 pool.query(`
 SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
 FROM teachers
@@ -20,4 +35,17 @@ ORDER BY teacher;
   res.rows.forEach(row => {
     console.log(`${row.cohort}: ${row.teacher}`);
   })
-});
+}).catch(err => console.error('query error', err.stack));
+const cohortName = process.argv[2];
+const limit = process.argv[3] || 5;
+// Store all potentially malicious values in an array.
+const values = [`%${cohortName}%`, limit];
+const queryString = `
+  SELECT teachers.id as teachers_id, teachers.name as name, cohort.name as cohort
+  FROM teachers
+  JOIN cohorts ON cohort.id = cohort_id
+  WHERE cohorts.name LIKE $1
+  LIMIT $2;
+  `;
+
+  pool.query(queryString, values);
